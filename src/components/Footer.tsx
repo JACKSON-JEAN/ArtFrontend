2bc88d6f9acdfb8d0cb334825a5b99cc9149b251
JACKSON-JEAN <jacksonuwizeye@gmail.com>
2025-08-11 14:21:45 +0300

    corrected facebook link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -28,7 +28,7 @@ const Footer = () => {
               <a href="https://x.com/zubart716" className=" text-base py-1 font-thin capitalize hover:text-slate-200">Twitter</a>
             </li>
             <li className=" mt-1">
-              <a href="https://www.facebook.com/share/1BEZ86baZB/" className=" text-base py-1 font-thin capitalize hover:text-slate-200">FaceBook</a>
+              <a href="https://www.facebook.com/ZubArt" className=" text-base py-1 font-thin capitalize hover:text-slate-200">FaceBook</a>
             </li>
             <li className=" mt-1">
               <a href="https://www.instagram.com/zubart29" className=" text-base py-1 font-thin capitalize hover:text-slate-200">Instagram</a>
