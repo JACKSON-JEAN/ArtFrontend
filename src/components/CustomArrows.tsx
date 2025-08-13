3cb06ab2c9ca26cfc4f73a4daf08952e07ecd661
JACKSON-JEAN <jacksonuwizeye@gmail.com>
2025-08-13 23:59:54 +0300

    New changes

--- src/components/CustomArrows.tsx
+++ src/components/CustomArrows.tsx
@@ -25,7 +25,7 @@ export const NextArrow: React.FC<ArrowProps> = ({ onClick }) => (
   // Custom prev arrow
 export  const PrevArrow: React.FC<ArrowProps> = ({ onClick }) => (
     <div
-      className="absolute top-1/2 -left-3 bg-white border p-.5 transform -translate-y-1/2 cursor-pointer text-xl text-red-950 hover:text-red-900 z-10"
+      className="absolute top-1/2 -left-3 bg-white border p-0.5 transform -translate-y-1/2 cursor-pointer text-xl text-red-950 hover:text-red-900 z-10"
       onClick={onClick}
     >
       <PrevArrowIcon />
