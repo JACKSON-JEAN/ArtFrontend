5a73661e5b2b24950afe3be4f36396c13e8e86b6
JACKSON-JEAN <jacksonuwizeye@gmail.com>
2025-08-20 13:57:15 +0300

    new changes

--- src/components/AddRating.tsx
+++ src/components/AddRating.tsx
@@ -7,13 +7,14 @@ interface AddRatingProps{
 
 const AddRating: React.FC<AddRatingProps> = ({itemId}) => {
   return (
-    <div className=" w-36 absolute left-0 top-4 bg-white border shadow-sm px-2 py-1">
-      <p className=" text-base text-blue-600 hover:underline cursor-pointer">
-        Add Ratings
+    <div className=" w-36 absolute left-0 top-4 bg-white rounded-sm border shadow-sm">
+      <p className=" w-full text-base text-blue-600 hover:underline cursor-pointer !py-1 px-2">
+        <Link to="/reviews">Reviews</Link>
+        
       </p>
-      <Link to="/reviews" className=" text-base text-blue-600 hover:underline cursor-pointer">
-        View Ratings
-      </Link>
+      <p className=" text-base text-blue-600 hover:underline cursor-pointer py-1 px-2 border-t">
+        Add Reviews
+      </p>
     </div>
   );
 };
