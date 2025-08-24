c56b6803bcbe6d6c683d341f80954e82e9962426
JACKSON-JEAN <jacksonuwizeye@gmail.com>
2025-08-24 21:44:14 +0300

    new changes

--- src/pages/ArtItem.tsx
+++ src/pages/ArtItem.tsx
@@ -72,6 +72,7 @@ const ArtItem = () => {
     return <p>Invalid art ID. Please check the URL and try again.</p>;
   }
 
+  if (loading) return <div className={`${"wrapper"} w-full px-10 sm:px-16 min-h-screen py-4 bg-slate-50`}><p>Loading...</p></div>;
   if (error)
     return (
       <div className=" w-full px-10 sm:px-16 min-h-screen py-4 bg-slate-50">
@@ -82,7 +83,6 @@ const ArtItem = () => {
     <div
       className={`${"wrapper"} w-full px-10 sm:px-16 min-h-screen py-4 bg-slate-50`}
     >
-      {loading && <p>Loading...</p>}
       {addReview && <AddReviews artId={artwork.id} onClose={() => setAddReview(false) }/>}
       <div className={`${"artItem"} flex mb-6`}>
         <div className=" flex-1">
