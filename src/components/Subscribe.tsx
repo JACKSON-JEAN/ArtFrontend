783ab9a0968bfa47df564459a6b8cdbb48fd0359
JACKSON-JEAN <jacksonuwizeye@gmail.com>
2025-08-25 15:20:41 +0300

    new changes

--- src/components/Subscribe.tsx
+++ src/components/Subscribe.tsx
@@ -35,7 +35,7 @@ const Subscribe = () => {
     addSubscriber({
       variables: {
         addSubscriberInput: {
-          email: userEmail
+          email: userEmail.trimEnd()
         } 
       }
     })
