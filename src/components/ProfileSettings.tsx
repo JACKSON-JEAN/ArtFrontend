eb9e93cd1fb01786db0ac91c521d307f8e0b36c0
JACKSON-JEAN <jacksonuwizeye@gmail.com>
2025-12-30 19:03:50 +0300

    Profile settings

--- src/components/ProfileSettings.tsx
+++ src/components/ProfileSettings.tsx
@@ -67,7 +67,7 @@ const ProfileSettings: React.FC<ProfileProps> = ({ onClose }) => {
 
   return (
     <div
-      className={`${"profile-login"} absolute top-7 -right-[50px] lg:-right-[18px] shadow-xl border w-auto bg-white rounded-sm !z-40`}
+      className={`${"profile-login"} absolute top-7 -right-[50px] lg:-right-[18px] shadow-xl border min-w-48 w-auto bg-white rounded-sm !z-40`}
     >
       <div ref={profileRef} className=" relative px-3 py-2">
         {userEmail && <p className=" text-sm mb-1">{userEmail}</p>}
